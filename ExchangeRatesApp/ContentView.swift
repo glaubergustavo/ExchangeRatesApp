5ee516a993f1dd25885e45cdb644e313ae0eeb2c
Glauber Gustavo <glauber.gustavo@madeinweb.com.br>
2023-10-20 20:16:05 -0300

    Initial Commit

---
+++ ExchangeRatesApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExchangeRatesApp
+//
+//  Created by madeinweb on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExchangeRatesApp/ExchangeRatesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesAppApp.swift
+//  ExchangeRatesApp
+//
+//  Created by madeinweb on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
