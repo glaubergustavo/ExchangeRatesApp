fd501aa0517937fea4aa4392d7c3b831362118f7
Glauber Gustavo <glauber.gustavo@madeinweb.com.br>
2023-10-21 20:32:13 -0300

    Adding currency fluctuation view

---
+++ ExchangeRatesApp/Extensions.swift
@@ -0,0 +1,57 @@
+//
+//  Extensions.swift
+//  ExchangeRatesApp
+//
+//  Created by madeinweb on 21/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Double {
+    
+    var color: Color {
+        if self.sign == .minus {
+            return .red
+        } else {
+            return .green
+        }
+    }
+    
+    func formatter(decimalPlaces: Int, with changeSymbol: Bool = false) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.roundingMode = .halfUp
+        numberFormatter.minimumFractionDigits = (decimalPlaces > 2) ? decimalPlaces : 2
+        numberFormatter.maximumFractionDigits = (decimalPlaces > 2) ? decimalPlaces : 2
+        numberFormatter.locale = Locale(identifier: "pt_BR")
+        
+        guard let value = numberFormatter.string(from: NSNumber(value: self)) else {
+            return String(self)
+        }
+        
+        if changeSymbol {
+            if self.sign == .minus {
+                return "\(value)"
+            } else {
+                return "+\(value)"
+            }
+        }
+        
+        return value.replacingOccurrences(of: "-", with: "")
+    }
+    
+    func toPercentage(with changeSymbol: Bool = false) -> String {
+        let value = formatter(decimalPlaces: 2)
+        
+        if changeSymbol {
+            if self.sign == .minus {
+                return "\u{2193} \(value)%"
+            } else {
+                return "\u{2191} \(value)%"
+            }
+        }
+        
+        return "\(value)%"
+    }
+}
---
+++ ExchangeRatesApp/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExchangeRatesApp
+//
+//  Created by madeinweb on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatesFluctuationView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
